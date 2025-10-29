c54703c9f1849f53c86c1d2550f9822be0336bbb
Aggelos Georgiadis <ag.gewr@gmail.com>
2025-10-29 09:48:26 +0200

    Initial commit

---
+++ BookLibrary/BookLibraryApp.swift
@@ -0,0 +1,32 @@
+//
+//  BookLibraryApp.swift
+//  BookLibrary
+//
+//  Created by Aggelos Georgiadis on 29/10/25.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct BookLibraryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ BookLibraryTests/BookLibraryTests.swift
@@ -0,0 +1,16 @@
+//
+//  BookLibraryTests.swift
+//  BookLibraryTests
+//
+//  Created by Aggelos Georgiadis on 29/10/25.
+//
+
+import Testing
+
+struct BookLibraryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
